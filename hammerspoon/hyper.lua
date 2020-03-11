98e588c587d237fe3917d6e5f9ffad69579f9460
aesadde <albertosadde@gmail.com>
2020-03-11 10:14:03 -0700

    feat(hm): hammerspoon updates

---
+++ hammerspoon/hyper.lua
@@ -0,0 +1,22 @@
+
+
+hyper = hs.hotkey.modal.new({}, "F17")
+
+
+-- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
+function enterHyperMode()
+  hyper.triggered = false
+  hyper:enter()
+end
+
+-- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
+-- send ESCAPE if no other keys are pressed.
+function exitHyperMode()
+  hyper:exit()
+  if not hyper.triggered then
+    hs.eventtap.keyStroke({}, 'ESCAPE')
+  end
+end
+
+-- Bind the Hyper key
+f18 = hs.hotkey.bind({}, "cmd", "alt", "ctrl", "shift", enterHyperMode, exitHyperMode)
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,25 +1,29 @@
--- global
-local hyper = {"cmd", "alt", "ctrl","shift"}
-local log = hs.logger.new('hammerspoon','debug')
-
 ----------------------------------------------------------------------------------------------------
 -- Load Dependencies
 require "wifi"
 require "airpods"
--- require "reminders"
+local spotify = require "spotify"
+
+
+----------------------------------------------------------------------------------------------------
+-- Global Settings
+local log = hs.logger.new('hammerspoon','debug')
+local hyper = {"cmd", "alt", "ctrl","shift"}
+
+hs.hotkey.alertDuration=0
+hs.hints.showTitleThresh = 0
+hs.window.animationDuration = 0
 
 -- Reload config
-hs.hotkey.bind(hyper, "R", function()
-  hs.reload()
-end)
+
+hs.hotkey.bind(hyper, "R", "Reload Configuration", function() hs.reload() end)
+
 hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
 
 ----------------------------------------------------------------------------------------------------
 -- Load Spoons
 if not hspoon_list then
     hspoon_list = {
-        "HSearch",
-        "WinWin",
     }
 end
 
@@ -30,8 +34,6 @@ end
 
 ----------------------------------------------------------------------------------------------------
 -- Window Management
-hs.window.animationDuration = 0
-
 hs.hotkey.bind(hyper, "h", function()
   local win = hs.window.focusedWindow();
   if not win then return end
@@ -55,6 +57,8 @@ end)
 
 hs.hotkey.bind(hyper, "f", function() hs.hints.windowHints() end)
 
+-- hs.hotkey.bind(table.concat(hyper, "X"), "L", function() hs.caffeinate.lockScreen() end)
+
 ----------------------------------------------------------------------------------------------------
 -- App shortcuts
 local applicationHotkeys = {
@@ -74,7 +78,7 @@ end
 
 ----------------------------------------------------------------------------------------------------
 -- Connect Airpods
-hs.hotkey.bind(hyper, "x", function()
+hs.hotkey.bind(table.concat(hyper, "B"), "P", function()
   local ok, output = airPods("Alberto’s AirPods Pro")
   if ok then
     hs.alert.show(output)
@@ -84,6 +88,18 @@ hs.hotkey.bind(hyper, "x", function()
 end)
 
 ----------------------------------------------------------------------------------------------------
--- Reminders
--- FIXME: NOT WORKING
--- hs.hotkey.bind(hyper, "t", addReminder)
+-- Spotify Keys
+--
+hs.hotkey.bind({}, "f7", function()
+  spotify.previousNotify()
+end)
+
+hs.hotkey.bind({}, "f8", function()
+  spotify.toggle()
+end)
+
+hs.hotkey.bind({}, "f9", function()
+  spotify.nextNotify()
+end)
+
+
