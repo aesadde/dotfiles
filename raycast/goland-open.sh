61e3cac49da3a1cd3497c3ad83ee2124d6f420d1
aesadde <albertosadde@gmail.com>
2021-11-16 14:42:28 -0500

    fix: improve goland-open script

--- raycast/goland-open.sh
+++ raycast/goland-open.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Dependency: Goland (https://www.jetbrains.com/go/)
 
@@ -12,11 +12,38 @@
 # @raycast.packageName System
 # @raycast.schemaVersion 1
 
-# @raycast.argument1 { "type": "text", "placeholder": "Directory..." }
+# @raycast.argument1 { "type": "text", "placeholder": "project name" }
+# @raycast.argument2 { "type": "text", "optional": true, "placeholder": "Base Dir..."  }
 
-if ! command -v goland &> /dev/null; then
-	echo "goland command is required (https://www.jetbrains.com/go/).";
-	exit 1;
+# goland is installed in this path, raycast doesn't allow login shells
+export PATH=$PATH:/Users/aesadde/.local/bin
+
+if [ -z "$1" ]; then
+  echo "First arg can't be blank"
+  exit 1
+fi
+GLOB="*$1*"
+
+BASE_DIR="/Users/aesadde/repos"
+if [ -n "$2" ]; then
+  BASE_DIR="$2"
 fi
 
-goland "/Users/aesadde/repos/akorda/${1}"
+if ! command -v goland &>/dev/null; then
+  echo "goland command is required (https://www.jetbrains.com/go/)."
+  exit 1
+fi
+
+if ! command -v fd &>/dev/null; then
+  echo "fd is required (install with brew install fd)"
+  exit 1
+fi
+
+repo=$(fd --maxdepth 2 --type d --glob "$GLOB" $BASE_DIR)
+
+if [ -z "$repo" ]; then
+  echo "No repo found."
+  exit 1
+fi
+
+goland "$repo"
